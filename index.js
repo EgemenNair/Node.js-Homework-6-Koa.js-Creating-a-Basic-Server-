eeec9d17c2e1e7ff5355a555a744a27973f30a27
EgemenNair <nairegemen@gmail.com>
2022-11-16 15:25:01 +0300

    Setting the responses

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Koa Imported:
-import Koa from 'koa';
+import Koa from "koa";
 
 // Koa defined to 'app' constant:
 const app = new Koa();
@@ -9,3 +9,22 @@ const port = 3000;
 
 // Listening for port:
 app.listen(port);
+
+// Responses:
+app.use((ctx) => {
+    switch (ctx.request.url) {
+        case "/index":
+            ctx.response.body = "<h1>Index Page</h1>";
+            break;
+        case "/about":
+            ctx.response.body = "<h1>About Page</h1>";
+            break;
+        case "/contact":
+            ctx.response.body = "<h1>Contact Page</h1>";
+            break;
+        default:
+            ctx.response.status = 404;
+            ctx.response.body = "<h1>404 NOT FOUND</h1>";
+            break;
+    }
+});
